dbe18312c4473ce9c6b00fc523063d08ac9e59cd
shahed hossain <shahed0hossain@gmail.com>
2023-02-26 15:26:24 +0600

    my order page issue fixed

--- src/Components/Pages/MyOrder/MyOrder.js
+++ src/Components/Pages/MyOrder/MyOrder.js
@@ -1,3 +1,4 @@
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
@@ -19,7 +20,13 @@ const MyOrder = () => {
         authorization: `Bearer ${localStorage.getItem('accessToken')}`
       }
     })
-      .then(res => res.json())
+      .then(res => {
+        if (res.status === 401 || res.status === 403) {
+          signOut(auth);
+          return
+        }
+        return res.json()
+      })
       .then(data => setOrder(data))
 
   }, [user]);
